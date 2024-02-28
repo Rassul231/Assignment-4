20fbeb1f2eaba0ea4fe2d858354dc544f337fac2
Rassul231 <153492109+Rassul231@users.noreply.github.com>
2024-02-28 11:28:10 +0600

    Update UserController.java

--- DoxbinRDM_2/src/main/java/com/doxbinrdm_2/controller/UserController.java
+++ DoxbinRDM_2/src/main/java/com/doxbinrdm_2/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
