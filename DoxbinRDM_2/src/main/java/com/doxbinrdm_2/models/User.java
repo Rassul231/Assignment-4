50e09d2e49d96ef12702d975dfbcac06cf771240
Rassul231 <153492109+Rassul231@users.noreply.github.com>
2024-02-28 11:28:45 +0600

    Update User.java

--- DoxbinRDM_2/src/main/java/com/doxbinrdm_2/models/User.java
+++ DoxbinRDM_2/src/main/java/com/doxbinrdm_2/models/User.java
@@ -14,7 +14,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
     @Column(nullable = false, unique = true)
     private String login;
